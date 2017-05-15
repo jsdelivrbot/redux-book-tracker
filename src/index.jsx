d7e1225e643843b87356ea2fccce88f8f6f48642
Aki Gao <xag9bb@virginia.edu>
2017-05-14 21:18:09 -0400

    complete reducers

--- src/index.jsx
+++ src/index.jsx
@@ -11,40 +11,12 @@ const DEFAULT_TERM = 'Japan'
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: null,
-     };
-
-     this.videoSearch(DEFAULT_TERM);
-
-  }
-
-  videoSearch(term) {
-    YTSearch({
-      key: API_KEY,
-      term: term
-    },
-    videos => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-    });
   }
 
   render() {
-    const videoSearch = _.debounce((term) => this.videoSearch(term), 300);
     return (
       <div>
-        <SearchBar
-          defaultValue={DEFAULT_TERM}
-          onSearchTermChange={term => videoSearch(term)}/>
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList
-          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
-          videos={this.state.videos} />
+
       </div>
     );
   }
