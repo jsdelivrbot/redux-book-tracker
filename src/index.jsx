1e75808a4f3b3b44a6cb340abec3705efc3bfddb
Aki Gao <xag9bb@virginia.edu>
2017-05-14 21:29:42 -0400

    complete containers - connecting redux to react & continued, add containers, add export to books reducer, clean up index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,5 @@
-import _ from 'Lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_details'
 
 const API_KEY = 'AIzaSyDOZVmlJpsTZy9ioUo3yXILgvbPH7cO3d4';
 const DEFAULT_TERM = 'Japan'
